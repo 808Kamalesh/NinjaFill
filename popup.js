33a46bb4b326293ad287429cbb95bee201099f44
808Kamalesh <154341138+808Kamalesh@users.noreply.github.com>
2024-05-01 01:21:42 +0530

    Updated

--- popup.js
+++ popup.js
@@ -4,11 +4,11 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   document.getElementById('autofillButton').addEventListener('click', function() {
-    chrome.storage.sync.get(['name', 'address', 'email', 'phone'], function(data) {
+    chrome.storage.sync.get(['name', 'address', 'email', 'phone', 'linkedin', 'portfolio', 'workExperience', 'education', 'skills'], function(data) {
       console.log('Sending autofill data:', data);
       chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, { action: 'autofill', data: data });
       });
     });
   });
-});
+})
