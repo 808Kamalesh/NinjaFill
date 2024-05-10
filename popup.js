c8ac80ab41797f9dd9330181d4645a08a55e1098
808Kamalesh <154341138+808Kamalesh@users.noreply.github.com>
2024-05-11 03:33:26 +0530

    Updated

--- popup.js
+++ popup.js
@@ -1,27 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const darkModeToggle = document.getElementById('darkModeToggle');
-    const body = document.body;
-    const container = document.querySelector('.container');
-  
-    darkModeToggle.addEventListener('change', () => {
-      body.classList.toggle('dark-mode');
-      container.classList.toggle('dark-mode');
-    });
-  
-    const clickSound = new Audio('click.mp3');
-  
-    document.getElementById('infoButton').addEventListener('click', function() {
-      clickSound.play();
-      chrome.tabs.create({ url: chrome.runtime.getURL('info.html') });
-    });
-  
-    document.getElementById('autofillButton').addEventListener('click', function() {
-      clickSound.play();
-      chrome.storage.sync.get(['name', 'dateOfBirth', 'gender', 'nationality', 'maritalStatus', 'email', 'phone', 'alternatePhone', 'linkedin', 'website', 'github', 'education', 'major', 'gpa', 'coursework', 'workExperience', 'internships', 'volunteer', 'certifications', 'skills', 'technicalSkills', 'softSkills', 'language'], function(data) {
-        console.log('Sending autofill data:', data);
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-          chrome.tabs.sendMessage(tabs[0].id, { action: 'autofill', data: data });
-        });
+  const darkModeToggle = document.getElementById('darkModeToggle');
+  const body = document.body;
+  const container = document.querySelector('.container');
+
+  darkModeToggle.addEventListener('change', () => {
+    body.classList.toggle('dark-mode');
+    container.classList.toggle('dark-mode');
+  });
+
+  const clickSound = new Audio('click.mp3');
+
+  document.getElementById('infoButton').addEventListener('click', function() {
+    chrome.tabs.create({ url: chrome.runtime.getURL('info.html') });
+  });
+
+  document.getElementById('autofillButton').addEventListener('click', function() {
+    clickSound.play();
+    chrome.storage.sync.get(['name', 'dateOfBirth', 'gender', 'nationality', 'maritalStatus', 'email', 'phone', 'alternatePhone', 'linkedin', 'website', 'github', 'education', 'major', 'gpa', 'coursework', 'workExperience', 'internships', 'volunteer', 'certifications', 'skills', 'technicalSkills', 'softSkills', 'language', 'projects', 'projectDescription', 'projectLinks' ], function(data) {
+      console.log('Sending autofill data:', data);
+      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { action: 'autofill', data: data });
       });
     });
   });
+});
